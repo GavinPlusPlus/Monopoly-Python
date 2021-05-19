16ecfa32091b14fe2a241046d1b8c507c6802b91
Gavin Kerr <kerrgv@gmail.com>
2021-05-19 16:46:19 -0700

    More Work on Board

--- Source/GameBoard.py
+++ Source/GameBoard.py
@@ -17,31 +17,48 @@ class GameBoard:
     #Functions
     def __init__(self):
 
+        relPath = "\\json\\board.json"
+        absPath = os.path.abspath("") + relPath
         try:
-            relPath = "\\Source\\json\\board.json"
-            absPath = os.path.abspath("") + relPath
             with open(absPath, "r") as file:
                 self.boardJSON = json.load(file)
             print("[Info]: Loaded board.json, applying settings...")
         except FileNotFoundError:
             print("[Error]: Failed to load board.json! Exiting...")
+            print(absPath)
             sys.exit(-1)
 
     def printRawJson(self):
         print(self.boardJSON)
 
     def printCurrentBoard(self):
-        #Print top
+
         stringToPrint = ""
+        centerSpacing = "                                                                        "
+
+        #Print Sides
+        for i in range (0, 9):
+            for j in range (0, 5):
+                stringToPrint = ""
+                #Left Side
+                stringToPrint += str(self.boardJSON["board"]["left"][str(8 - i)]["color"])
+                tileStr = self.boardJSON["board"]["left"][str(8 - i)]["tile"]
+                stringToPrint += tileStr.split(";")[j] 
+                stringToPrint += centerSpacing
+
+                #Right Side
+                stringToPrint += str(self.boardJSON["board"]["right"][str(i)]["color"])
+                tileStr = self.boardJSON["board"]["right"][str(i)]["tile"]
+                stringToPrint += tileStr.split(";")[j] 
+                print(stringToPrint)
+
+        #Print bottom
         for i in range (0, 5):
             stringToPrint = ""
             for j in range (0, 11):
                 stringToPrint += str(self.boardJSON["board"]["bottom"][str(10 - j)]["color"])
                 tileStr = self.boardJSON["board"]["bottom"][str(10 - j)]["tile"]
                 stringToPrint += tileStr.split(";")[i]
-
-                
-
             print(stringToPrint)
 
 game = GameBoard()
